25fb520554e1251f12b73dc020db0564cef4785d
Makerimages <Makerimagesgames@gmail.com>
2015-04-03 13:34:55 +0300

    Debug messages in Pit::init()

--- src/OSZin/modules/PIT.cpp
+++ src/OSZin/modules/PIT.cpp
@@ -1,13 +1,16 @@
 #include "OSZin/modules/PIT.hpp"
 #include "libc/IOAccess.hpp"
+#include "OSZin/kernel/KernelGlobals.hpp"
 Pit::Pit() {
 
 }
 
 void Pit::init(int freq) {
+	tm.kputsf("Trying to initialize PIT to %d Hz.\n",freq);
 	int divisor = PIT_FREQUENCY / freq;
    	outb(PIT_CMD, CMD_BINARY | CMD_MODE3 | CMD_RW_BOTH | CMD_COUNTER0);
     outb(PIT_COUNTER0, divisor );
     outb(PIT_COUNTER0, divisor >> 8);
+    tm.kputs("Initialization routine ran. \n");
 }
 
