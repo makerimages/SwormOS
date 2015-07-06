6bea0e6774dad0b26381c8e09b97c435b34c00fe
Makerimages <makerimagesgames@gmail.com>
2015-07-06 21:25:59 +0300

    Last 2 are out of bounds

--- kernel/mem/pmm_new.c
+++ kernel/mem/pmm_new.c
@@ -2,13 +2,17 @@
 #include <textmode.h>
 #include <stdint.h>
 void pmm_new_init(uintptr_t start_addr,uint32_t length_bytes) {
-    kprintf(" length %d\n",length_bytes);
-    uint32_t frames;
+
     uintptr_t start = ROUND_TO_PAGES(start_addr);
     uint32_t mem_length_bytes = length_bytes & ~PAGE_SIZE;
-    for (uintptr_t addr = start; addr <= start+mem_length_bytes;addr+=PAGE_SIZE) {
-        frames++;
+
+    for (void * addr = start; addr < start+mem_length_bytes;addr+=PAGE_SIZE) {
+        addr = (struct frame*) addr;
+        struct frame* frame = addr;
+        if(addr< (start+mem_length_bytes)) {
+            frame->next = (struct frame*)addr+PAGE_SIZE;
+        }
+        kprintf("PMM Page at 0x%x, next: 0x%x\n",frame,frame->next);
     }
-    kprintf("%d\n",frames);
 
 };
